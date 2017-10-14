38805e099c42a0c5a60530f6d931e2f0ad1c9b15
jm <woop@tuta.io>
2017-10-14 14:49:39 +0200

    man i'm bad at feature based commits :( There is now tabs and sums for the receipt. you can also remove items from the receipt now.

--- cashier/static/main.js
+++ cashier/static/main.js
@@ -1,49 +1,71 @@
-
-let receipt_state = [];
+let receipt_state = {"sum": 0};
 
 function already_listed_handler(identifier) {
 
-    let already_listed = false;
+    if (receipt_state[identifier]) {
+        const receipt_item = $("li[data-id='" + identifier + "']");
 
-    $("#receipt").children().each(function () {
-        if (Number($(this).attr("data-id")) === identifier) {
-            let new_ammount;
-            $(this).find("[data-ammount]").text(function (i, oldText) {
-                new_ammount = Number(oldText) + 1;
-                return new_ammount
-            });
-            let price = receipt_state.find(x => x.id === identifier).price;
-            console.log(price);
-            $(this).find("[data-price]").text(new_ammount*price);
-            already_listed = true;
+        receipt_state[identifier]['amount'] += 1;
+        const amount = receipt_state[identifier]['amount'];
+        receipt_item.find("[data-ammount]").text(amount);
+
+        const price = receipt_state[identifier]['price'];
+        const new_price = (amount * price).toFixed(2);
+        receipt_item.find("[data-price]").text(new_price);
+
+        return true;
+    }
+    return false;
+}
+
+function update_receipt_sum() {
+    let sum = 0;
+    Object.keys(receipt_state).forEach(function (key, index) {
+        if (key === "sum") {
+            return;
+        }
+        if ('price' in receipt_state[key]) {
+            let price = receipt_state[key]["price"];
+            let amount = receipt_state[key]["amount"];
+            sum += (price * amount);
         }
     });
-
-    return already_listed;
+    receipt_state['sum'] = sum;
+    console.log(receipt_state);
+    $("#sum").text(sum.toFixed(2))
 }
 
 function add_new_item_to_receipt(identifier) {
+
     $.getJSON("/get/item/" + identifier, function (result) {
 
-        let receipt = $("#receipt");
+        const receipt = $("#receipt");
         receipt.append(
-            "<li data-id="
-            + result['id']
-            + "><dl><dt>Amount: <span data-ammount>1</span></dt><dt>Item: "
-            + result['title']
-            + "</dt><dd>Price: <span data-price>"
-            + result['price']
-            + "</span>€</dd></dl></li>");
+            "<li data-id=" + result['id'] + "><dl>"
+            + "<dt>Amount: <span data-ammount>1</span></dt>"
+            + "<dt>Item: " + result['title'] + "</dt>"
+            + "<dd>Price: <span data-price>" + result['price'].toFixed(2) + "</span>€</dd>"
+            + "<button data-remove>Remove</button></dl></li>"
+        );
 
-        receipt_state.push(result);
+        receipt.on('click', "li[data-id='" + identifier + "']", function () {
+            delete receipt_state[identifier];
+            $("[data-id=" + identifier + "]").remove();
+            update_receipt_sum();
+        });
+
+        result['amount'] = 1;
+        receipt_state[identifier] = result;
+        update_receipt_sum();
     });
 
 }
 
-function update_receipt_with(identifier) {
-    if (!already_listed_handler(identifier)){
+function add_to_receipt(identifier) {
+    if (!already_listed_handler(identifier)) {
         add_new_item_to_receipt(identifier)
     }
+    update_receipt_sum();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -51,14 +73,30 @@ document.addEventListener('DOMContentLoaded', function () {
     contrast();
     $('.itemTitle').click(function () {
 
-        update_receipt_with(Number($(this).attr('id')))
+        add_to_receipt(Number($(this).attr('id')))
 
     });
 
+    $('#finishProcess').click(function () {
+
+        $(".items").hide();
+        $("#finishProcessTab").show();
+
+    });
+
+    $('#closeInteraction').click(function () {
+
+        $("#finishProcessTab").hide();
+        $(".items").show();
+
+    })
+
 }, false);
 
-
-// https://codepen.io/znak/pen/aOvMOd
+/*
+    Contrast fix by
+    https://codepen.io/znak/pen/aOvMOd
+*/
 function contrast() {
 
     let C, L, rgb;
